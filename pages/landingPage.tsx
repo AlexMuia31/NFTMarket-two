a54fac79a0a376453029ca51999767fc771e633e
AlexMuia31 <alexmuia31@gmail.com>
2022-07-26 13:34:29 +0300

    front end

---
+++ pages/landingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "./Components/Banner";
+
+function landingPage() {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+}
+
+export default landingPage;
