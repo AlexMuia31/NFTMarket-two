9cd616337c556bc10a5b056e6ab7a69853702755
AlexMuia31 <alexmuia31@gmail.com>
2022-07-26 13:47:34 +0300

    landing page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "./Components/Banner";
+import Banner2 from "./Components/Banner2";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Banner />
+      <Banner2 />
     </div>
   );
 };
